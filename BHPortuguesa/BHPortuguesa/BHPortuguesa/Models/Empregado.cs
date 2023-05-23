52d66f93b5fa4dd440fced69907464d312e846e5
Miguel Florentino <aluno21596@ipt.pt>
2023-05-23 20:02:18 +0100

    Finishing the Models

--- BHPortuguesa/BHPortuguesa/BHPortuguesa/Models/Empregado.cs
+++ BHPortuguesa/BHPortuguesa/BHPortuguesa/Models/Empregado.cs
@@ -1,6 +1,14 @@
-﻿namespace BHPortuguesa.Models
+﻿namespace BHPortuguesa.Models;
+using System.ComponentModel.DataAnnotations;
+
+public class Empregado
 {
-    public class Emepregado
-    {
-    }
+    [Key]
+    public int IDEmpregado { get; set; }
+
+    [Required]
+    [StringLength(75)]
+    public string NomeEmpregado { get; set; }
 }
+
+
